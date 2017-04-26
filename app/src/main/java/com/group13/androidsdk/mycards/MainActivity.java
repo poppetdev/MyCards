ba5f0c72760ea6f0f25402f6ac82bfe30a4035bb
tweedot <tweeoneil@gmail.com>
2017-04-25 21:39:10 -0400

    Main menu

--- app/src/main/java/com/group13/androidsdk/mycards/MainActivity.java
+++ app/src/main/java/com/group13/androidsdk/mycards/MainActivity.java
@@ -16,14 +16,47 @@
 
 package com.group13.androidsdk.mycards;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
+    Button btnReview, btnBrowse, btnSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReview = (Button)findViewById(R.id.btnReview);
+        btnBrowse = (Button)findViewById(R.id.btnBrowse);
+        btnSettings = (Button)findViewById(R.id.btnSettings);
+
+
+
+    }
+
+
+    public void btnClick(View v){
+        switch(v.getId()){
+            case R.id.btnStReview:
+                //go to START REVIEW activity
+                Intent toReview = new Intent(this, ReviewCategoryActivity.class);
+                startActivity(toReview);
+                break;
+            case R.id.btnBrowse:
+                //go to BROWSE CARDS activity
+                Intent toBrowse = new Intent(this, BrowseCardsActivity.class);
+                startActivity(toBrowse);
+                break;
+            case R.id.btnSettings:
+                //to to SETTINGS activity
+                Intent toSettings = new Intent(this, SettingsActivity.class);
+                startActivity(toSettings);
+                break;
+            default:
+                break;
+        }
     }
 }
